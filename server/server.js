4a319d38ed0756f778302fdc3e16bce3481100d3
Praveen Saw <praveen.kumar.paytm2019@gmail.com>
2024-03-09 13:01:20 +0530

    Fix the cors bugs

--- server/server.js
+++ server/server.js
@@ -1,13 +1,14 @@
-
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
 import authRoutes from "./routes/auth.routes.js";
 import recordsRoutes from "./routes/record.routes.js";
 
 const PORT = process.env.PORT || 5000;
+const __dirname = path.resolve();
 dotenv.config();
 const app = express();
 app.use(express.json()); // to parse the incoming requests with JSON payloads (from req.body)
@@ -16,6 +17,13 @@ app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/records", recordsRoutes);
+
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
+
 
 app.listen(PORT, () => {
 	connectToMongoDB();
